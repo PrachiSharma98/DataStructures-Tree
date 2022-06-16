eeca2760a9d6ca48837c178dd5362f2a19efba37
PrachiSharma98 <99522178+PrachiSharma98@users.noreply.github.com>
2022-06-16 13:01:58 +0530

    Create IntersectionOfTwoLinkedLust.java

---
+++ IntersectionOfTwoLinkedLust.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if(headA==null || headB==null) return null;
+        ListNode a =headA;
+        ListNode b =headB;
+        while(a!=b)
+        {
+            a = a==null?headB:a.next;
+            b= b==null?headA:b.next;
+        }
+        return a;
+    }
+}
+
+
+
+Given the heads of two singly linked-lists headA and headB, return the node at which the two lists intersect. If the two linked lists have no intersection at all, return null.
+
+For example, the following two linked lists begin to intersect at node c1:
+
